0c57d62de833b2aafd5d98a49bc3c3321fdd8d52
k5nn <kennydelrosario1996@gmail.com>
2026-03-24 16:55:53 +0800

    Added Generic Card Layout System

--- components/card/card.gd
+++ components/card/card.gd
@@ -4,18 +4,10 @@ class_name Card
 signal card_highlighted( inst : Card , cfg : Dictionary )
 signal card_deselected( inst : Card , cfg : Dictionary )
 
-const suit_assets = {
-	"Provenance" : preload("res://assets/Card/Provenance.png") , 
-	"Utility" : preload("res://assets/Card/Utility.png") , 
-	"Durability" : preload("res://assets/Card/Durability.png") , 
-	"Craftsmanship" : preload("res://assets/Card/Craftsmanship.png") 
-}
-
-const dot_assets = {
-	"R" : preload( "res://assets/Card/modifier.png" ) ,
-	"G" : preload( "res://assets/Card/modifier.png" ) ,
-	"B" : preload( "res://assets/Card/modifier.png" ) ,
-	"Y" : preload( "res://assets/Card/modifier.png" ) ,
+const layouts = {
+	"Quality" : preload( "res://components/card/negotiation_layout.tscn" ) ,
+	"Battle" : true ,
+	"Negotiation" : true
 }
 
 var local_cfg := {}
@@ -32,69 +24,48 @@ func get_default_cfg() -> Dictionary:
 	}
 #create
 
-#read
-func validate_suit( suit : String ) :
-	if !suit_assets.has( suit ) :
-		printerr( "invalid suit" )
-		get_tree().quit( 1 )
-
-func validate_modifier( modifiers : Array ) :
-	if modifiers.size() > 3 :
-		printerr( "too many modifiers" )
-		get_tree().quit( 2 )
-	
-	for modifier in modifiers :
-		if !dot_assets.has( modifier ) :
-			printerr( "Invalid dot modifier" )
-			get_tree().quit( 3 )
-#read
-
 #update
-func add_dots( modifiers ) :
-	for child in $DotContainer.get_children() :
-		child.queue_free()
-	
-	for modifier in modifiers :
-		var dot = TextureRect.new()
-		dot.texture = dot_assets[ modifier ]
-		$DotContainer.add_child( dot )
-
 func verify_and_set_cfg( cfg ) -> void :
 	
-	const valid_modes = { "quality" : true , "battle" : true , "negotiation" : true }
-	
-	if !valid_modes.has( cfg.mode ) :
+	if !layouts.has( cfg.mode ) :
 		printerr( "invalid mode" )
 		get_tree().quit( 1 )
-		
-	if cfg.mode == "quality" :
-		validate_suit( cfg.suit )
-		if cfg.has( "dot_modifier" ) :
-			validate_modifier( cfg.modifiers )
+	
+	if layouts[ cfg.mode ] is PackedScene :
+		local_cfg.set( "instance" , layouts[ cfg.mode ].instantiate() )
+		local_cfg.instance.add_assets( {
+			"Provenance" : preload("res://assets/Card/Provenance.png") , 
+			"Utility" : preload("res://assets/Card/Utility.png") , 
+			"Durability" : preload("res://assets/Card/Durability.png") , 
+			"Craftsmanship" : preload("res://assets/Card/Craftsmanship.png") ,
+			"R" : preload( "res://assets/Card/modifier.png" ) ,
+			"G" : preload( "res://assets/Card/modifier.png" ) ,
+			"B" : preload( "res://assets/Card/modifier.png" ) ,
+			"Y" : preload( "res://assets/Card/modifier.png" ) ,
+		} )
+		add_child( local_cfg.instance )
 	
 	local_cfg = cfg
-	apply_cfg( cfg )
+	#apply_cfg( cfg )
 
-func apply_cfg( cfg ) -> void :
-	
-	if cfg.mode == "quality" :
-		$SuitIcon.texture = suit_assets[ cfg.suit ]
-		$Multiplier.text = "x+" + str( cfg.plus_multiplier )
-		$BaseValue.text = str( cfg.base_value )
-		add_dots( local_cfg.modifiers )
+#func apply_cfg( cfg ) -> void :
+	#
+	#if cfg.mode == "quality" :
+		
+		#add_dots( local_cfg.modifiers )
 
-func update_suit( target_suit : String ) :
-	validate_suit( target_suit )
-	local_cfg.suit = target_suit
-	$SuitIcon.texture = suit_assets[ target_suit ]
-
-func update_base_value( base_value : int ) :
-	local_cfg.base_value = base_value
-	$BaseValue.text = str( base_value )
-
-func update_multiplier( plus_multiplier : int ) :
-	local_cfg.plus_modifier = plus_multiplier
-	$Multiplier.text = "x+" + str( plus_multiplier )
+#func update_suit( target_suit : String ) :
+	#validate_suit( target_suit )
+	#local_cfg.suit = target_suit
+	#$SuitIcon.texture = suit_assets[ target_suit ]
+#
+#func update_base_value( base_value : int ) :
+	#local_cfg.base_value = base_value
+	#$BaseValue.text = str( base_value )
+#
+#func update_multiplier( plus_multiplier : int ) :
+	#local_cfg.plus_modifier = plus_multiplier
+	#$Multiplier.text = "+x" + str( plus_multiplier )
 #update
 
 #events
